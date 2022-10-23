e3df774940d2a2758fc1bd90c865d0b217438996
aimandesu <aimanafiqbinesam@gmail.com>
2022-10-23 19:34:40 +0800

    add new feature in commen part

--- lib/screen/posts/post_comment.dart
+++ lib/screen/posts/post_comment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class PostComment extends StatefulWidget {
   const PostComment({super.key});
@@ -9,6 +10,7 @@ class PostComment extends StatefulWidget {
 
 class _PostCommentState extends State<PostComment> {
   bool isExpanded = false;
+  final PanelController _pc = PanelController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,106 +19,124 @@ class _PostCommentState extends State<PostComment> {
       bottom: 0,
       left: 0,
       right: 0,
-      child: Container(
-        height: isExpanded ? size.height * 0.4 : size.height * 0.05,
-        width: size.width * 1,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(25),
+      child: SlidingUpPanel(
+        backdropEnabled: true,
+        onPanelOpened: () => setState(() {
+          isExpanded = true;
+        }),
+        onPanelClosed: () => setState(() {
+          isExpanded = false;
+        }),
+        controller: _pc,
+        collapsed: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(25),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () => _pc.open(),
+                icon: const Icon(Icons.comment),
+              ),
+            ],
           ),
         ),
-        child: isExpanded
-            ? Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(25),
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                        height: size.height * 0.05,
-                        child: IconButton(
-                          onPressed: () => setState(() {
-                            isExpanded = !isExpanded;
-                          }),
-                          icon: const Icon(Icons.arrow_back_ios),
-                        ),
+        // color: Colors.white,
+        minHeight: size.height * 0.05,
+        maxHeight: size.height * 0.4,
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(25),
+        ),
+        panel:
+            //  isExpanded
+            //     ?
+            Stack(
+          children: [
+            isExpanded
+                ? Positioned(
+                    top: 0,
+                    left: 0,
+                    child: SizedBox(
+                      height: size.height * 0.05,
+                      child: IconButton(
+                        onPressed: () => _pc.close(),
+                        icon: const Icon(Icons.arrow_back_ios),
                       ),
                     ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: size.height * 0.29,
-                            child: ListView.builder(
-                              padding: EdgeInsets.zero,
-                              itemCount: 10,
-                              itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text("comment $index"),
-                                );
-                              },
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.all(5),
-                            padding: const EdgeInsets.symmetric(horizontal: 5),
-                            height: size.height * 0.06,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black),
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(25),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: SizedBox(
-                                    width: size.width * 0.9,
-                                    child: TextFormField(
-                                      decoration:
-                                          const InputDecoration.collapsed(
-                                        hintText: "Comment",
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const IconButton(
-                                  onPressed: null,
-                                  icon: Icon(Icons.send),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            : Row(
+                  )
+                : Container(),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
                 children: [
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                    icon: Icon(Icons.comment),
+                  SizedBox(
+                    height: size.height * 0.27,
+                    child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text("comment $index"),
+                        );
+                      },
+                    ),
                   ),
-                  Icon(Icons.heart_broken),
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    height: size.height * 0.06,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            width: size.width * 0.9,
+                            child: TextFormField(
+                              decoration: const InputDecoration.collapsed(
+                                hintText: "Comment",
+                              ),
+                              maxLines: null,
+                            ),
+                          ),
+                        ),
+                        const IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.send),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
+            )
+          ],
+        ),
+
+        // : Row(
+        //     children: [
+        //       IconButton(
+        //         onPressed: () {
+        //           setState(() {
+        //             isExpanded = !isExpanded;
+        //           });
+        //         },
+        //         icon: Icon(Icons.comment),
+        //       ),
+        //       Icon(Icons.heart_broken),
+        //     ],
+        //   ),
       ),
     );
   }
--- lib/screen/posts/posts.dart
+++ lib/screen/posts/posts.dart
@@ -9,7 +9,7 @@ class Posts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    // Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: Responsive(
       mobile: Stack(
